13a742e5c3c0c62246ec5d7448922fc1033d0ac7
Irfan-KV <79312738+Irfan-KV@users.noreply.github.com>
2022-09-23 15:09:20 +0530

    Update UserScript.swift

--- ios/Classes/Types/UserScript.swift
+++ ios/Classes/Types/UserScript.swift
@@ -15,7 +15,6 @@ public class UserScript : WKUserScript {
     private var _contentWorld: Any? = nil
 
     @available(iOS 14.0, *)
-    lazy var contentWorld: WKContentWorld = WKContentWorld.page
     var contentWorld: WKContentWorld {
         get {
             return (_contentWorld as? WKContentWorld) ?? WKContentWorld.page
